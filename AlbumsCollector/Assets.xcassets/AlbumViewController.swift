17aeb16f55d18d478054701ea5f8737f9a10ef54
azdigital <andrew@azdigital.ca>
2016-11-14 21:05:11 -0500

    All elements of VC are connected

---
+++ AlbumsCollector/Assets.xcassets/AlbumViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AlbumViewController.swift
+//  AlbumsCollector
+//
+//  Created by Andrew on 2016-11-14.
+//  Copyright © 2016 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    @IBOutlet var albumImageView: UIImageView!
+    
+    @IBOutlet var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: AnyObject) {
+    }
+  
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+}
